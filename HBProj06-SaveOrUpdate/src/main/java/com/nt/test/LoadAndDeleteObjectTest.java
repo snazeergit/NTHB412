8a65ead88023e398743ec168398c379954c62d2a
Nazeer <nazeersd.java@gmail.com>
2022-06-12 23:30:02 +0530

    SaveOrUpdate Object

---
+++ HBProj06-SaveOrUpdate/src/main/java/com/nt/test/LoadAndDeleteObjectTest.java
@@ -0,0 +1,53 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.nt.entity.Product;
+
+//Using Try with Resource introduced in Java 9
+public class LoadAndDeleteObjectTest {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException {
+		//Bootstarp / Activate the hibernate
+		Configuration cfg = new Configuration();
+
+		//specify the  hibernate cfg file name and location
+		cfg.configure("com/nt/cfgs/hibernate.cfg.xml");
+
+		//build SessionFactory having all services specified in cfg file and  mapping file
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		// create Session obj 
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try (factory; session) {//Introduced from java 9
+			//Prepare entity object
+			Product prod = new Product();
+			prod.setPid(1003);
+			prod.setPname("tablespoon");
+			prod.setPrice(58.5f);
+			prod.setQty(1.0f);
+
+			tx = session.beginTransaction();
+			session.saveOrUpdate(prod);
+
+			tx.commit();
+			System.out.println("Record is saved or updated");
+
+		} // The SessionFactory, Session objs will be closed here automatically
+		catch (HibernateException he) {
+			he.printStackTrace();
+			if (tx != null && tx.getRollbackOnly() && tx.getStatus() != null) {
+				//Roll back the current resource transaction. 
+				tx.rollback();
+				System.out.println("Record is not saved or updated");
+			}
+		}
+	}
+
+}
